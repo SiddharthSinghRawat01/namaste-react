0408d3734d2458a92cea0997ff15ef104612086b
Siddharth Singh Rawat <siddharthsinghrawat01@gmail.com>
2026-01-17 20:52:26 +0530

    ignite our app

--- App.js
+++ App.js
@@ -12,14 +12,18 @@
 </div>
 
 */
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 tag."),
-    React.createElement("h2", {}, "I'm h2 tag."),
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent", key: 'parent' }, [
+  React.createElement("div", { id: "child", key: 'child' }, [
+    React.createElement("h1", {key: 'h1'}, "I'm h1 tag."),
+    React.createElement("h2", {key: 'h2'}, "I'm h2 tag."),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm h1 tag."),
-    React.createElement("h2", {}, "I'm h2 tag."),
+  React.createElement("div", { id: "child2", key: 'child2' }, [
+    React.createElement("h1", {key: 'h1'}, "I'm h1 tag."),
+    React.createElement("h2", {key: 'h2'}, "I'm h2 tag."),
   ]),
 ]);
 const heading = React.createElement(
---
+++ README.md
@@ -0,0 +1,5 @@
+# parcel
+- dev build
+- Local Server
+- HME = Hot Module Replacement
+- File Watching Algorithium - written in C++
