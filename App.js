238767903c74b50a0e4569ac98fc6467d9a3d84a
Siddharth Singh Rawat <siddharthsinghrawat01@gmail.com>
2026-01-21 10:36:09 +0530

    React Component

--- App.js
+++ App.js
@@ -2,16 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // React Element
+const Title = () => (
+  <h1 id="heading" className="head">
+    Namste React using JSX
+  </h1>
+);
 
-const heading = React.createElement("h1", { id: "heading" }, "Namste React!");
+// React Component
 
-// JSX - it is a HTML-like or XML-like syntax
-// JSX (transpiled before it reaches to JS engine) - PARCEL - BABLE( balbles job is to trnsple the code)
-
-const jxsHeading = <h1 id="heading" className="head">Namste React using JSX</h1>;
-
-console.log({ heading, jxsHeading });
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 className="heading"> Namste React Functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jxsHeading);
+root.render(<HeadingComponent />);
